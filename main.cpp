f7c4922597a607297d1e07246fe1d3a74cdbc743
Ayushhurkat22 <2021it24ay@mitsgwl.ac.in>
2024-07-03 17:21:10 +0530

    Added Models

--- main.cpp
+++ main.cpp
@@ -1,7 +1,68 @@
 #include <iostream>
+#include "Model/RubiksCube3dArray.cpp"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    RubiksCube3dArray object3DArray;
+    object3DArray.print();
+
+    if(object3DArray.isSolved())
+    {
+        cout<<"SOLVED\n";
+    }
+    else
+    {
+        cout<<"NOT SOLVED\n";
+    }
+
+    // cout<<"\n";
+    // cout<<"Move L called ->\n";
+    object3DArray.l();
+    object3DArray.print();
+    if(object3DArray.isSolved())
+    {
+        cout<<"SOLVED\n";
+    }
+    else
+    {
+        cout<<"NOT SOLVED\n";
+    }
+
+    object3DArray.lPrime() ;
+    object3DArray.print();
+    if(object3DArray.isSolved())
+    {
+        cout<<"SOLVED\n";
+    }
+    else
+    {
+        cout<<"NOT SOLVED\n";
+    }
+    // object3DArray.print();
+    //
+    // cout<<"\n";
+    // cout<<"Random Shuffle Function called ->\n";
+    // object3DArray.randomShuffleCube(3);
+    // object3DArray.print();
+
+    cout<<"\n";
+    cout<<"Random Shuffle Function called ->\n";
+    vector<RubiksCube::MOVE> movesToShuffle = object3DArray.randomShuffleCube(3);
+    cout<<"Series of moves applied: ";
+    for(auto move: movesToShuffle)
+    {
+        cout<<object3DArray.getMove(move) <<" ";
+    }
+    cout<<"\n";
+    object3DArray.print();
+
+    if(object3DArray.isSolved())
+    {
+        cout<<"SOLVED\n";
+    }
+    else
+    {
+        cout<<"NOT SOLVED\n";
+    }
     return 0;
 }
